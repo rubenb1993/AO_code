6c71d663d5c351a5eaf616f5b69543ce1cfe2006
Ruben Biesheuvel <rbiesheuvel@tudelft.net>
2016-09-09 16:51:55 +0200

    added defocus function

--- mirrortest.py
+++ mirrortest.py
@@ -7,6 +7,30 @@ mirror = edac40.OKOMirror("169.254.158.203") # Enter real IP in here
 
 voltages = 6.0 * np.ones(19)  # V = 0 to 12V
 mirror.set(voltages)
-time.sleep(5)
+
+
+def actuators_test():
+    for i in range(19):
+        print i
+        raw_input("next?")
+        voltages = np.ones(19)*6.0
+        mirror.set(voltages)
+        voltages[i]+=6.0
+        time.sleep(1)
+        mirror.set(voltages)
+
+def defocus_cycle():
+    i=1
+    while 1:
+        i+=1
+        voltage=np.sin(i%100*2*np.pi/100.0)*6.0
+        voltages=np.ones(19)*voltage+6.0
+        voltages[4]=6.0
+        voltages[7]=6.0
+        mirror.set(voltages)
+##        print np.sin(i%100*2*np.pi/100.0)*6.0
+        time.sleep(0.05)
+##actuators_test()        
+defocus_cycle()   
 
 mirror.close()
