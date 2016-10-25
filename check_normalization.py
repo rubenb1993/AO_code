fixed 2 and sign for janssen method

check_normalization.py
@@ -25,7 +25,7 @@ dx = x[1] - x[0]
 xx, yy = np.meshgrid(x, y)
 rho, theta = cart2pol(xx, yy)
 j_max = 7
-n, m = Zn.Zernike_j_2_nm(np.arange(2,j_max+2))
+n, m = Zn.Zernike_j_2_nm(np.arange(1,j_max+2))
 Cnm = Zn.complex_zernike(j_max, xx, yy)
 mask = [xx**2 + yy**2 >= 1]
 xn, yn = np.ma.array(xx, mask = mask), np.ma.array(yy, mask = mask)
@@ -33,20 +33,23 @@ tiled_mask = np.tile(mask, (Cnm.shape[2], 1,1)).T
 Cnm_mask = np.ma.array(Cnm, mask = tiled_mask)
 A = dx**2 * np.sum(Cnm_mask * np.conj(Cnm_mask), axis = (0,1))/np.pi
 
-power_mat = Zn.Zernike_power_mat(j_max+1)
-j = np.arange(2, j_max+2)
+power_mat = Zn.Zernike_power_mat(j_max+2)
+j = np.arange(2, j_max+3)
 Znm = Zn.Zernike_xy(xx, yy, power_mat, j)
 Znm_mask = np.ma.array(Znm, mask = tiled_mask)
 A2 = dx**2 * np.sum(Znm_mask * Znm_mask, axis = (0,1)) / np.pi
 
+v_max = np.max(Znm_mask[...,4]) 
+v_min = np.min(Znm_mask[...,4]) 
+
 fig, axarr = plt.subplots(2,2, sharex = True, sharey = True, figsize = plt.figaspect(1.))
-axarr[0,0].contourf(xn, yn, Znm_mask[...,3], rstride=1, cstride=1, cmap=cm.YlOrBr, linewidth = 0)
-axarr[0,0].set_title('Z_5')
-axarr[1,0].contourf(xn, yn, Znm_mask[...,4], rstride=1, cstride=1, cmap=cm.YlOrBr, linewidth = 0)
-axarr[1,0].set_title('Z_6')
-axarr[0,1].contourf(xn, yn, np.sqrt(3)*np.real(Cnm_mask[...,3]), rstride=1, cstride=1, cmap=cm.YlOrBr, linewidth = 0)
-axarr[0,1].set_title('Re(C_5)')
-plot = axarr[1,1].contourf(xn, yn, np.sqrt(3)*np.imag(Cnm_mask[...,3]), rstride=1, cstride=1, cmap=cm.YlOrBr, linewidth = 0)
+axarr[0,0].contourf(xn, yn, Znm_mask[...,4], rstride=1, cstride=1, cmap=cm.YlOrBr, linewidth = 0, vmin=v_min, vmax=v_max)
+axarr[0,0].set_title('Z_6')
+axarr[1,0].contourf(xn, yn, Znm_mask[...,3], rstride=1, cstride=1, cmap=cm.YlOrBr, linewidth = 0, vmin=v_min, vmax=v_max)
+axarr[1,0].set_title('Z_5')
+axarr[0,1].contourf(xn, yn, -np.sqrt(2*n[5]+2)*np.imag(Cnm_mask[...,5]), rstride=1, cstride=1, cmap=cm.YlOrBr, linewidth = 0, vmin=v_min, vmax=v_max)
+axarr[0,1].set_title('-Im(C_6)')
+plot = axarr[1,1].contourf(xn, yn, np.sqrt(2*n[5]+2)*np.real(Cnm_mask[...,4]), rstride=1, cstride=1, cmap=cm.YlOrBr, linewidth = 0, vmin=v_min, vmax=v_max)
 axarr[1,1].set_title('Im(C_5)')
 print(n, m)
 fig.subplots_adjust(right = 0.8)

check_radial_janssen.py
@@ -114,17 +114,17 @@ Cnm = complex_zernike(j_max, x, y)
 print(Cnm.shape)
 
     
-rho = np.linspace(-1, 1, 100)
+rho = np.linspace(-1, 1, 5000)
 rho2 = 2*rho**2 - 1
 theta = np.zeros(rho.shape)
-j = np.arange(2, 30)
+j = np.array([1, 10, 30, 50, 63, 110, 135, 1050, 1052])
 n, m = Zn.Zernike_j_2_nm(j)
 nm2 = (n - np.abs(m))/2
-f, axarr = plt.subplots(14, 1, sharex = True, sharey = False)
+f, axarr = plt.subplots(len(j), 1, sharex = True, sharey = False)
 
 
-R_janss = np.zeros((100, len(j)))
-R_zern = np.zeros((100, len(j)))
+R_janss = np.zeros((len(rho), len(j)))
+R_zern = np.zeros((len(rho), len(j)))
 list_of_poly = []
 a = np.zeros(len(nm2))
 R_janss = jacobi_pol(nm2, a, np.abs(m), m, rho, rho2)
@@ -132,11 +132,10 @@ for i in range(len(j)):
     #list_of_poly.append(spec.jacobi(nm2[i], 0, np.abs(m[i])))
     #poly_i = spec.jacobi(nm2[i], 0, np.abs(m[i]))
     #R_janss[:, i] = poly_i(rho2)
-    if i%2 == 0:
-        R_zern[:, i] = Zernike_nm(n[i], m[i], rho, theta)
-        axarr[i/2].plot(rho, R_zern[:,i])
-        axarr[i/2].plot(rho, R_janss[:,i])
-        axarr[i/2].set_ylabel([str(n[i]), str(m[i])])
+    R_zern[:, i] = Zernike_nm(n[i], m[i], rho, theta)
+    axarr[i].plot(rho, R_zern[:,i])
+    axarr[i].plot(rho, R_janss[:,i])
+    axarr[i].set_ylabel([str(n[i]), str(m[i])])
 
 plt.show()
 print(list_of_poly)

close_control_loop.py
@@ -102,7 +102,7 @@ r_sh_px = r_sh_m / px_size_sh
 x = np.linspace(1, nx, nx)
 y = np.linspace(1, ny, ny)
 xx, yy = np.meshgrid(x, y)
-j_max= 10           # maximum fringe order
+j_max= 20           # maximum fringe order
 ### Normalize x, y
 wavelength = 632e-9
 #r_sh_m = 1.924e-3 #physical radius
@@ -168,18 +168,18 @@ if np.any(np.sqrt(x_pos_norm_f **2 + y_pos_norm_f**2) > (1+35/r_sh_px)):
 
 
 G = LSQ.matrix_avg_gradient(x_pos_norm_f, y_pos_norm_f, j_max, r_sh_px)
-G_old = LSQ.geometry_matrix_2(x_pos_norm_f, y_pos_norm_f, j_max, r_sh_px)
 a = np.zeros(j_max)
-#a[0] = -0.3 * wavelength
-#a[1] = -0.1 * wavelength
-a[2] = 1 * wavelength
-#a[3] = 0.2 * wavelength
-#a[5] = 0.3 * wavelength
-#a[6] = 0.3 * wavelength
-#a[7] = 0.3 * wavelength
-#a[8] = 0.2 * wavelength
-#a[9] = 0.3 * wavelength
-#a[5] = 0.3 * wavelength
+ind = 4
+n_ind, m_ind = Zn.Zernike_j_2_nm(ind+2)
+if m_ind + 1 > n_ind - 1:
+    print('this was edge case 1')
+elif np.abs(m_ind - 1.0) > np.abs(n_ind - 1.0):
+    print('this was edge case 2')
+else:
+    print('this was no edge case')
+
+a[ind] = 0.3 * wavelength
+
 
 u_dm_flat = u_dm
 v_abb = abberations2voltages(G, V2D_inv, a, f_sh, r_sh_m, px_size_sh)
@@ -206,7 +206,6 @@ interferogram = np.fliplr(interferogram)
 #plt.show()
 raw_input("re-cover the reference mirror")
 a_measured_new = LSQ.LSQ_coeff(x_pos_zero_f, y_pos_zero_f, G, image_control, sh, px_size_sh, r_sh_px, f_sh, j_max)
-a_measured_old = LSQ.LSQ_coeff(x_pos_zero_f, y_pos_zero_f, G_old, image_control, sh, px_size_sh, r_sh_px, f_sh, j_max)
 a_janss_real, a_janss_check = janssen.coeff(x_pos_zero, y_pos_zero, image_control, sh, px_size_sh, f_sh, r_sh_m, j_max)
 
 ##
@@ -214,7 +213,7 @@ a_janss_real, a_janss_check = janssen.coeff(x_pos_zero, y_pos_zero, image_contro
 f, axarr = plt.subplots(2, 2, figsize=(plt.figaspect(1.)))
 axarr[0,0].set_title('interferogram of wanted abberation')
 Zn.plot_interferogram(j_max, a, axarr[0,0])
-axarr[0, 1].imshow(interferogram, cmap = 'bone')
+axarr[0, 1].imshow(interferogram[120:120+2*r_sh_px, 320:320+2*r_sh_px], cmap = 'bone')
 axarr[0, 1].set_title('measured interferogram')
 Zn.plot_interferogram(j_max, a_measured_new, axarr[1,0])
 axarr[1, 0].set_title('interferogram simulated from LSQ')
@@ -223,17 +222,17 @@ axarr[1,1].set_title('interferogram simulated from Janssen')
 
 f2 = plt.figure()
 ax = f2.add_subplot(1,1,1)
-indexs = np.arange(1, j_max+1, 1)
+indexs = np.arange(2, j_max+2, 1)
 ax.plot(indexs, a/wavelength, 'ro', label = 'intended')
 ax.plot(indexs, a_measured_new/(wavelength), 'bo', label = 'LSQ solution')
 #ax.plot(indexs, np.real(a_janss_check)/(wavelength), 'go', label = 'measured_janss_check')
-ax.plot(indexs, a_janss_real/wavelength, 'ko', label = 'Janssen solution')
-ax.set_xlim([0, j_max+1])
+ax.plot(indexs, np.real(a_janss_check)/wavelength, 'ko', label = 'Janssen solution')
+ax.set_xlim([0, j_max+2])
 ax.legend(loc = 'best')
 ax.set_xlabel('Coeffcient number')
 ax.set_ylabel('a_j [\lambda]')
 
 plt.show()
-
+print(a_measured_new/a_janss_real)
 
 #a_janss_real, a_janss_check = janssen.coeff(x_pos_zero, y_pos_zero, image_control, sh, px_size_sh, f_sh, r_sh_m, j_max)

janssen.py
@@ -57,8 +57,10 @@ def coeff(x_pos_zero, y_pos_zero, zero_image, sh, px_size, f, r_sh_m, j_max,):
 
     # Make Zernike matrix
     kmax = np.power(np.ceil(np.sqrt(j_max)),2) #estimation of maximum fringe number
+    print('jmax = ' + str(j_max) + 'kmax = ' + str(kmax))
     n, m = Zn.Zernike_j_2_nm(np.array(range(1, int(kmax)+1))) #find n and m pairs for maximum fringe number
     Kmax = np.max(Zn.Zernike_nm_2_j(n+1, np.abs(m)+1)) #find highest order of j for which beta is needed
+    print('Kmax = ' + str(Kmax))
     #power_mat = Zn.Zernike_power_mat(Kmax+1)
     Z_mat = Zn.complex_zernike(Kmax, x_pos_norm, y_pos_norm)
 ##    for jj in range(2, int(Kmax)+1):
@@ -107,11 +109,11 @@ def coeff(x_pos_zero, y_pos_zero, zero_image, sh, px_size, f, r_sh_m, j_max,):
         if m > 0:
             a_real[jj-2] = (1.0/np.sqrt(2*n+2)) * np.real(a[jj-1])
             j_min = Zn.Zernike_nm_2_j(n, -m)
-            a_check[jj-2] = (1.0/np.sqrt(2*n+2))*(a[jj-1] + a[j_min-1])/2.0
+            a_check[jj-2] = (1.0/np.sqrt(2*n+2))*(a[jj-1] + a[j_min-1])
         elif m < 0:
             j_plus = Zn.Zernike_nm_2_j(n, np.abs(m))
             a_real[jj-2] = (1.0/np.sqrt(2*n+2)) * np.imag(a[j_plus - 1])
-            a_check[jj-2] = np.sqrt(2*n+2) * (a[j_plus - 1] - a[jj-1])/(2.0 * 1j)
+            a_check[jj-2] = (1.0/np.sqrt(2*n+2)) * (a[j_plus - 1] - a[jj-1]) * 1j
         else:
             a_real[jj-2] = (1.0/np.sqrt(n+1)) * np.real(a[jj-1])
             a_check[jj-2] = (1.0/np.sqrt(n+1)) * a[jj-1]